5826b2f09eb146fc4d7d8de876cfa8662c6ea64d
pip64 <76626915+pip64@users.noreply.github.com>
2025-11-15 22:14:24 +0700

    Added unicode transliteration for google registered usernames

--- internal/service/generate_username.go
+++ internal/service/generate_username.go
@@ -3,9 +3,14 @@ package service
 import (
 	"fmt"
 	"math/rand"
+	"regexp"
 	"strings"
 	"time"
+
+	"github.com/mozillazg/go-unidecode"
 )
+
+var latinLetters = regexp.MustCompile("[^a-zA-Z]+")
 
 func GenerateUsername(fullName string) string {
 	parts := strings.Fields(fullName)
@@ -13,19 +18,23 @@ func GenerateUsername(fullName string) string {
 	var firstName, lastName string
 
 	if len(parts) > 0 {
-		firstName = strings.ToLower(parts[0])
+		firstName = unidecode.Unidecode(parts[0])
+		firstName = strings.ToLower(latinLetters.ReplaceAllString(firstName, ""))
 	}
 	if len(parts) > 1 {
-		lastName = strings.ToLower(parts[1])
+		lastName = unidecode.Unidecode(parts[1])
+		lastName = strings.ToLower(latinLetters.ReplaceAllString(lastName, ""))
 	}
 
+	if firstName == "" {
+		firstName = "user"
+	}
 	if lastName == "" {
 		lastName = "user"
 	}
 
 	rand.Seed(time.Now().UnixNano())
 	randomNum := rand.Intn(10000)
-
 	randomStr := fmt.Sprintf("%04d", randomNum)
 
 	return fmt.Sprintf("%s_%s_%s", firstName, lastName, randomStr)
