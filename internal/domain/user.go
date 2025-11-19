43bceb2405554e25d25cf34501332cb805a2ea02
pip64 <76626915+pip64@users.noreply.github.com>
2025-11-19 20:16:06 +0700

    Added null strings to user struct

--- internal/domain/user.go
+++ internal/domain/user.go
@@ -5,7 +5,7 @@ import "time"
 type User struct {
 	ID          int       `db:"id" json:"id"`
 	Username    string    `db:"username" json:"username"`
-	Email       string    `db:"email" json:"email"`
-	DisplayName string    `db:"display_name" json:"display_name"`
+	Email       *string   `db:"email" json:"email"`
+	DisplayName *string   `db:"display_name" json:"display_name"`
 	Date        time.Time `db:"date" json:"date"`
 }
