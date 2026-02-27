199457c8224959f5e1d2dbdb2322665dd5089eef
Aquiren <aquiren@proton.me>
2026-02-27 18:12:21 +0500

    Add quiet mode

--- tests.py
+++ tests.py
@@ -1,6 +1,14 @@
 import customlib_testing as t
+import sys
 
-t.g("/ws") # A test line to test the tests.py tests
+show_not_added = True
+if "quiet" in sys.argv:
+    t.show_success = False
+    show_not_added = False
 
-t.list_missing()
+t.g("/ws")
+
+if show_not_added:
+    t.list_missing()
+
 t.run_tests()
